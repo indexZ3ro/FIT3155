19ffd3bb9882bb99a8ec8230c75312d926f2f984
indexZ3ro <101028812+indexZ3ro@users.noreply.github.com>
2023-05-04 10:44:28 +1000

    Update st2sa.py

--- st2sa.py
+++ st2sa.py
@@ -1,10 +1,11 @@
 def st2sa(input_str):
     input_list = [char for char in input_str]
     input_list.append('$')
-    
+
     sufTree = SuffixTree()
     sufTree.build(input_list)
-        
+
+
 class SuffixTree:
     class Node:
         def __init__(self, start):
@@ -12,8 +13,7 @@ class SuffixTree:
             self.children = []
             self.end = None
             self.sufLink = self
-        
-        
+
     def __init__(self):
         self.root = self.Node(None)
         self.active_node = self.root
@@ -21,48 +21,63 @@ class SuffixTree:
         self.remainder_length = 0
         self.last_j = 0
         self.global_end = 0
-        
+        self.active_edge_node = None
+
     def build(self, input_list):
         n = len(input_list)
-        for i in range(n-1):
+        for i in range(n - 1):
             self.global_end += 1
             if i == 0:
                 self.add_node(self.active_node, 0)
             else:
-                for j in range(self.last_j, i+2):
-                    match = False
-                    # NOTE: J = the tree up until the active node!
-                    # TODO: Change this so that we compare the next character (i+1) to the [active_node index + the remainder length] 
-                    # TODO: if we dont have an active edge iterate children and find if we have an active edge otherwise we can simply use the active edge
-                    #       if we have no match we need to add 
-                    # psudo code = 
-                       # if remainder exists: compare [remainder_index + remainder_len], [i+1]
-                       # else:
-                    for node in self.active_node.children:
-                        # compare [child.start], [i+1]
-                    
-                        if node_str == compare_str:
-                            match = True
-                    
-                    #rule(3)  
-                    if match:
-                        rule3()
-                    #rule 2: uses j to create the start index of new node && update last j
+                for j in range(self.last_j, i + 2):
+                    # skip count down to extension
+                    if(self.active_edge_node is not None):
+                        while self.remainder_length > self.get_node_length(self.active_node):
+                            self.remainder_length -= self.get_node_length(self.active_node)
+                            self.remainder_index += self.get_node_length(self.active_node)
+                            self.active_node = self.active_edge_node
+                            if(self.remainder_length > 0):
+                                for node in self.active_node.children:
+                                    if input_list[node.start] == input_list[self.remainder_index]:
+                                        self.active_edge_node = node
+                                        break
+                            else:
+                                self.active_edge_node = None
+                                self.remainder_index = None
+
+                    # rule 3
+                    if input_list[self.active_node.start + self.remainder_length] == input_list[i + 1]:
+                        self.remainder_length += 1
+                        if self.get_node_length(self.active_node) == self.remainder_length:
+                            self.active_node = self.active_edge_node
+                            self.active_edge_node = None
+                            self.remainder_length = 0
+                            self.remainder_index = None
+                        if self.previous_rule == 2:
+                            self.previous_node.sufLink = self.active_node
+                        break # showstopper
+                    # rule 2
                     else:
-                        rule2()
-            
-        
+                        self.active_node.children.remove(self.active_edge_node)
+                        new_node = self.add_node(self.active_node, j)
+                        new_node.children.append(self.active_edge_node)
+
     def add_node(self, parent, start_index):
-        child = self.Node(start_index) 
+        child = self.Node(start_index)
         parent.children.append(child)
-        
+        return child
+
     def get_node_end(self, node):
-            if node.end is None:
-                return self.global_end
-            else:
-                return node.self.end
-        
-        
+        if node.end is None:
+            return self.global_end
+        else:
+            return node.end
+
+    def get_node_length(self, node):
+        return self.get_node_end(node)-node.start
+
+
 st2sa("mississippi")
 
 
